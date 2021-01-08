ba778b4774c7545db27b74633ca5f6dae00b357d
ceren-askit <cerenaskit15@gmail.com>
2021-01-08 22:13:28 +0300

    Update views.py
    Co-Authored-By: Ali Gürcan Baki <76747214+AliGurcan@users.noreply.github.com>
    Co-Authored-By: ireemiinii <46257872+ireemiinii@users.noreply.github.com>

--- gmtlibrary/views.py
+++ gmtlibrary/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from gmtlibrary.models import *
+from django.core.files.storage import FileSystemStorage
 
 # Create your views here.
 def home(request, *args, **kwargs):
-    academican = Academician.objects.all()
+    academician = Academician.objects.all()
+    cname = Course.objects.all()
     context = {
-        'academican': academican,
+        'academician': academician,
+        'course': cname,
+
     }
     return render(request, 'gmtlibrary/home.html', context)
+
+# def courses(request, *args, **kwargs ):
+
+def upload(request):
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+    return render(request, 'gmtlibrary/upload.html', context)
