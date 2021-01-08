2376f26a97eba86a612026c8624e069ca9aa2911
ceren-askit <cerenaskit15@gmail.com>
2021-01-08 22:13:25 +0300

    Update urls.py
    Co-Authored-By: Ali Gürcan Baki <76747214+AliGurcan@users.noreply.github.com>
    Co-Authored-By: ireemiinii <46257872+ireemiinii@users.noreply.github.com>

--- gmtlibrary/urls.py
+++ gmtlibrary/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='gmtlibrary-home'),
+    # path('', views.courses, name='gmtlibrary-courses'),
+    path('upload/', views.upload, name = 'gmtlibrary-upload'),
 
-]
+] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
